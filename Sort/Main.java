31387e4d004c5b4cde9964790fd5f4a4e45756b5
Keni <petryakov8@gmail.com>
2025-04-21 11:36:15 +0400

    add DZ

--- Sort/Main.java
+++ Sort/Main.java
@@ -9,31 +9,46 @@ public class Main {
                 { 31, 18, 15, 12, 10, 8, 6, 4, 2, 1 },
                 { 51, 30, 10, 9, 8, 7, 6, 5, 2, 1 }
         };
-
         int[] nationalTeam = mergeAll(teams);
-        System.out.println(Arrays.toString(nationalTeam)); // [51, 45, 31, 31, 30, 24, 22, 20, 18, 17]
+        System.out.println(Arrays.toString(nationalTeam));
     }
 
     public static int[] mergeAll(int[][] teams) {
-
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>(10);
-
-        for (int[] team : teams) {
-            for (int rating : team) {
-                minHeap.offer(rating);
-                if (minHeap.size() > 10) {
-                    minHeap.poll();
-                }
+        PriorityQueue<Element> minHeap = new PriorityQueue<>(Comparator.comparingInt(e -> e.value));
+        for (int i = 0; i < teams.length; i++) {
+            if (teams[i].length > 0) {
+                minHeap.offer(new Element(teams[i][0], i, 0));
             }
         }
 
         int[] result = new int[10];
-        int index = 9;
+        int index = 0;
 
-        while (!minHeap.isEmpty()) {
-            result[index--] = minHeap.poll();
+        while (!minHeap.isEmpty() && index < 10) {
+            Element current = minHeap.poll();
+            result[index++] = current.value;
+
+            if (current.indexInArray + 1 < teams[current.arrayIndex].length) {
+                minHeap.offer(new Element(
+                        teams[current.arrayIndex][current.indexInArray + 1],
+                        current.arrayIndex,
+                        current.indexInArray + 1
+                ));
+            }
         }
 
         return result;
+    }
+
+    static class Element {
+        int value;
+        int arrayIndex;
+        int indexInArray;
+
+        public Element(int value, int arrayIndex, int indexInArray) {
+            this.value = value;
+            this.arrayIndex = arrayIndex;
+            this.indexInArray = indexInArray;
+        }
     }
 }
